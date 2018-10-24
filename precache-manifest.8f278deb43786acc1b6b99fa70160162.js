032960949c91e70e8c9e09fca4980d95ee6dcf9a
Bondarenko Yuriy <bondarenko.yuriy.1992@gmail.com>
2018-10-24 14:46:22 +0600

    Updates

---
+++ precache-manifest.8f278deb43786acc1b6b99fa70160162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "5c19470bd0a222de6f56",
+    "url": "/tic-tac-toe/static/js/main.5c19470b.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "/tic-tac-toe/static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "5c19470bd0a222de6f56",
+    "url": "/tic-tac-toe/static/css/main.33e094cf.chunk.css"
+  },
+  {
+    "revision": "c165456bf6c0b6788d6759804f06898e",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
